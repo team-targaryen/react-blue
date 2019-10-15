6f50786ca16e9847974bc255252f8696a1d31116
kendall <xxpker222@gmail.com>
2019-10-15 16:26:41 -0700

    added structure for testing

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              "@babel/transform-async-to-generator"
+            ]
           }
         }
       },
@@ -42,5 +46,8 @@ module.exports = {
         ]
       }
     ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
   }
 };
