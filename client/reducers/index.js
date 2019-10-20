d4feffccfdf5f8b0f8886289f164c382d2e2ff13
kendall <xxpker222@gmail.com>
2019-10-19 22:59:11 -0700

    fixed small issue with import statement

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-
 // import all reducers
 import mainReducer from './mainReducer';
-import './utils/reducerFunctions'
 //combine reducers
 const reducers = combineReducers({
   main: mainReducer
