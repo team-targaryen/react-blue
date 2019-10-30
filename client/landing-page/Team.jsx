fe1e7622f7c4063654d6fdf40ad107b300b7d5d9
kendall <xxpker222@gmail.com>
2019-10-29 21:21:04 -0700

    added profiles with button links, added some css styles

--- client/landing-page/Team.jsx
+++ client/landing-page/Team.jsx
@@ -1,35 +1,39 @@
 import React, { useState } from 'react';
 import GitHubButton from 'react-github-btn';
-
+import darren from '../assets/images/darren.jpeg';
+import kendall from '../assets/images/kendall.jpeg';
+import randy from '../assets/images/randy.jpeg';
+import krystal from '../assets/images/krystal.jpeg';
+import github from '../assets/images/githubLogo.png';
 const Team = () => {
   const [isCreatorProfiles] = useState([
     {
       name: 'Darren Zhu',
       githubHandle: 'Joodongri',
       github: 'https://github.com/Joodongri',
-      dataVanity: 'darrenDzhu',
-      linked: 'https://www.linkedin.com/in/darrenDzhu?trk=profile-badge'
+      linked: 'https://www.linkedin.com/in/darrenDzhu',
+      src: darren
     },
     {
       name: 'Kendall Lu',
       githubHandle: 'kendall-lu',
       github: 'https://github.com/kendall-lu',
-      dataVanity: 'kendall-lu',
-      linked: 'https://www.linkedin.com/in/kendall-lu?trk=profile-badge'
+      linked: 'https://www.linkedin.com/in/kendall-lu',
+      src: kendall
     },
     {
       name: 'Randy Reyes',
       githubHandle: 'rqreyes',
       github: 'https://github.com/rqreyes',
-      dataVanity: 'rqreyes',
-      linked: 'https://www.linkedin.com/in/rqreyes?trk=profile-badge'
+      linked: 'https://www.linkedin.com/in/rqreyes',
+      src: randy
     },
     {
       name: 'Krystal Chen',
       githubHandle: 'kcrystalchen',
       github: 'https://github.com/kcrystalchen',
-      dataVanity: 'kcrystalchen',
-      linked: 'https://www.linkedin.com/in/kcrystalchen?trk=profile-badge'
+      linked: 'https://www.linkedin.com/in/kcrystalchen',
+      src: krystal
     }
   ]);
   const creatorProfileArray = isCreatorProfiles.map(profile => {
@@ -39,23 +43,26 @@ const Team = () => {
         githubHandle={profile.githubHandle}
         name={profile.name}
         github={profile.github}
-        dataVanity={profile.dataVanity}
+        src={profile.src}
         linked={profile.linked}
       />
     );
   });
   return (
-    <React.Fragment>
+    <section className='team'>
       <h1>React-Blue</h1>
       <GitHubButtons />
       <Gifs />
-      {creatorProfileArray}
-    </React.Fragment>
+      <div className='profile-info'>
+        {creatorProfileArray}
+      </div>
+    </section>
   );
 };
 const GitHubButtons = () => {
   return (
     <div className='github-buttons'>
+      <img id='github-logo' src={github}></img>
       <GitHubButton
         href='https://github.com/team-targaryan/react-blue'
         data-size='large'
@@ -63,15 +70,7 @@ const GitHubButtons = () => {
       >
         Follow @React Blue
       </GitHubButton>
-      <GitHubButton
-        href='https://github.com/team-targaryan/react-blue'
-        data-size='large'
-        data-show-count='true'
-        aria-label='Star @React Blue on GitHub'
-      >
-        Star
-      </GitHubButton>
-      <button href='#'>Go To Our Web Application</button>
+      <a class="github-button" href="https://github.com/team-targaryan/react-blue" data-icon="octicon-star" aria-label="Star react-blue on GitHub">Star</a>
     </div>
   );
 };
@@ -83,40 +82,31 @@ const Gifs = () => {
         width='560'
         height='315'
         src='https://media.giphy.com/media/JpXEK9GC6Z9vk8lfDi/giphy.gif'
-        frameborder='0'
+        frameBorder='0'
         allowfullscreen
       ></iframe>
       <iframe
         width='560'
         height='315'
         src='https://media.giphy.com/media/Y3vwiphEodr52wcMrB/giphy.gif'
-        frameborder='0'
+        frameBorder='0'
         allowfullscreen
       ></iframe>
       <p>Insert Text Here</p>
     </div>
   );
 };
-const Profiles = ({ name, github, linked, dataVanity, githubHandle }) => {
+const Profiles = ({ name, github, linked, githubHandle, src }) => {
   return (
-    <div className='profile-info'>
-      <div
-        className='LI-profile-badge'
-        data-version='v1'
-        data-size='medium'
-        data-locale='en_US'
-        data-type='vertical'
-        data-theme='light'
-        data-vanity={dataVanity}
-      >
-        <a className='LI-simple-link' href={linked}>
-          {name}
-        </a>
+    <section>
+      <div className='individual-profile'>
+        <img id='profile-picture' src={src} />
+        <button id='linkedin' href={linked}><i className="fab fa-linkedin"></i>@{name}</button>
+        <GitHubButton id='github' href={github} data-size='large'>
+          Follow @{githubHandle}
+        </GitHubButton>
       </div>
-      <GitHubButton href={github} data-size='large'>
-        Follow @{githubHandle}
-      </GitHubButton>
-    </div>
+    </section>
   );
 };
 
